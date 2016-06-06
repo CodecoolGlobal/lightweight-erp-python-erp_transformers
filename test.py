9b03cbe1fd593bffebd3949700946ebc23059511
AbstractFlame <sami.gabor@gmail.com>
2016-06-06 06:42:48 +0200

    add test for common and ui

--- test.py
+++ test.py
@@ -74,6 +74,29 @@ def check_forbidden_functions(tester, file_name):
         tester.assertEqual(lines.find("input("), -1)
 
 
+def check_forbidden_list_functions(tester, file_name):
+    with open(file_name, "r") as file:
+        lines = file.read()
+        tester.assertEqual(lines.find("find("), -1)
+        tester.assertEqual(lines.find("sort("), -1)
+        tester.assertEqual(lines.find("sorted("), -1)
+        tester.assertEqual(lines.find("sum("), -1)
+        tester.assertEqual(lines.find("count("), -1)
+        tester.assertEqual(lines.find("index("), -1)
+
+
+class CommonTester(unittest.TestCase):
+
+    def test_forbidden_functions(self):
+        check_forbidden_functions(self, "common.py")
+
+
+class UITester(unittest.TestCase):
+
+    def test_forbidden_functions(self):
+        check_forbidden_list_functions(self, "ui.py")
+
+
 class AccountingTester(unittest.TestCase):
     data_file = "accounting/items_test.csv"
 
