5a3af244b3a3eccf28e1826176285fb7c7f82de0
Rudi Szabo <rudi.szabo@codecool.com>
2018-04-27 16:00:25 +0200

    fix docstrings and pep8 issues

--- data_analyser/data_analyser.py
+++ data_analyser/data_analyser.py
@@ -1,13 +1,14 @@
-# This module creates reports for marketing department.
-# This module can run independently from other modules.
-# Has no own datastructure but uses other modules.
-# Avoud using the database (ie. .csv files) of other modules directly.
-# Use the functions of the modules instead.
+"""
+This module creates reports for the marketing department.
+This module can run independently from other modules.
+Has no own data structure but uses other modules.
+Avoud using the database (ie. .csv files) of other modules directly.
+Use the functions of the modules instead.
+"""
 
 # todo: importing everything you need
 
 # importing everything you need
-import os
 import ui
 import common
 from sales import sales
@@ -17,8 +18,8 @@ from crm import crm
 def start_module():
     """
     Starts this module and displays its menu.
-    User can access default special features from here.
-    User can go back to main menu from here.
+     * User can access default special features from here.
+     * User can go back to main menu from here.
 
     Returns:
         None
@@ -34,12 +35,10 @@ def get_the_last_buyer_name():
     Returns the customer _name_ of the customer made sale last.
 
     Returns:
-        Customer name of the last buyer
+        str: Customer name of the last buyer
     """
 
     # your code
-
-    pass
 
 
 def get_the_last_buyer_id():
@@ -47,77 +46,61 @@ def get_the_last_buyer_id():
     Returns the customer _id_ of the customer made sale last.
 
     Returns:
-        Customer id of the last buyer
+        str: Customer id of the last buyer
     """
 
     # your code
-
-    pass
 
 
 def get_the_buyer_name_spent_most_and_the_money_spent():
     """
     Returns the customer's _name_ who spent the most in sum and the money (s)he spent.
-    Returns a tuple of customer name and the sum the customer spent.
-    eg.: (aH34Jq#&, 42)
 
-   Returns:
-        Tuple of customer name and the sum the customer spent
+    Returns:
+        tuple: Tuple of customer name and the sum the customer spent eg.: ('Daniele Coach', 42)
     """
 
     # your code
-
-    pass
 
 
 def get_the_buyer_id_spent_most_and_the_money_spent():
     """
     Returns the customer's _id_ who spent more in sum and the money (s)he spent.
-    Returns a tuple of customer id and the sum the customer spent.
-    eg.: (aH34Jq#&, 42)
 
-   Returns:
-        Tuple of customer id and the sum the customer spent
+    Returns:
+        tuple: Tuple of customer id and the sum the customer spent eg.: (aH34Jq#&, 42)
     """
 
     # your code
-
-    pass
 
 
 def get_the_most_frequent_buyers_names(num=1):
     """
-    Returns 'num' number of buyers (more precisely: the customers' name) who bought most frequently.
-    Returns an ordered list of tuples of customer names and the number of their sales.
-    (The first one bought the most frequent.)
-    eg.: [(aH34Jq#&, 8), (bH34Jq#&, 3)]
+    Returns 'num' number of buyers (more precisely: the customer's name) who bought most frequently in an
+    ordered list of tuples of customer names and the number of their sales.
 
     Args:
         num: the number of the customers to return.
 
     Returns:
-        Ordered list of tuples of customer names and num of sales
+        list of tuples: Ordered list of tuples of customer names and num of sales
+            The first one bought the most frequent. eg.: [('Genoveva Dingess', 8), ('Missy Stoney', 3)]
     """
 
     # your code
-
-    pass
 
 
 def get_the_most_frequent_buyers_ids(num=1):
     """
-    Returns 'num' number of buyers (more precisely: the customer ids of them) who bought more frequent.
-    Returns an ordered list of tuples of customer id and the number their sales.
-    (The first one bought the most frequent.)
-    eg.: [(aH34Jq#&, 8), (bH34Jq#&, 3)]
+    Returns 'num' number of buyers (more precisely: the customer ids of them) who bought more frequent in an
+    ordered list of tuples of customer id and the number their sales.
 
     Args:
         num: the number of the customers to return.
 
     Returns:
-        Ordered list of tuples of customer ids and num of sales
+        list of tuples: Ordered list of tuples of customer ids and num of sales
+            The first one bought the most frequent. eg.: [(aH34Jq#&, 8), (bH34Jq#&, 3)]
     """
 
     # your code
-
-    pass
