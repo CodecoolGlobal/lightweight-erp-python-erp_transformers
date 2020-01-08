46338394a9b9d5c63421a51dd7b0adaf0a97a6ca
baginski95 <bagiczny3@gmail.com>
2020-01-09 00:02:02 +0100

    Functions ramove,add in module sales work properly

--- sales/sales.py
+++ sales/sales.py
@@ -31,6 +31,50 @@ def start_module():
 
     # your code
 
+    options = ['show table',
+                'add',
+                'remove',
+                'update',
+                'get_lowest_price_item_id',
+                'get_items_sold_between']
+    dict_menu = {'1': show_table_wrapper,
+                '2': add_wrapper,
+                '3': remove_wrapper,
+                '4': update_wrapper,
+                '5': get_lowest_price_item_id,
+                '6': get_items_sold_between}
+    common.sub_menu(dict_menu, options, "Sales menu")
+
+
+def show_table_wrapper():
+    table = data_manager.get_table_from_file('sales/sales.csv')
+    show_table(table)
+
+
+def add_wrapper():
+    table = data_manager.get_table_from_file('sales/sales.csv')
+    add(table)
+
+
+def remove_wrapper():
+    table = data_manager.get_table_from_file('sales/sales.csv')
+    remove(table, ui.get_inputs(['ID :'], 'Enter ID: '))
+
+
+def update_wrapper():
+    table = data_manager.get_table_from_file('sales/sales.csv')
+    update(table, ui.get_inputs(['ID :'], 'Enter ID: '))
+
+
+def get_lowest_price_item_id():  # Need to be implemented
+    table = data_manager.get_table_from_file('sales/sales.csv')
+    get_lowest_price_item_id(table)
+
+
+def get_items_sold_between():  # Need to be implemented
+    table = data_manager.get_table_from_file('sales/sales.csv')
+    get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to)
+
 
 def show_table(table):
     """
@@ -58,7 +102,11 @@ def add(table):
     """
 
     # your code
-
+    ID_INDEX = 0
+    record = ui.get_inputs(['title: ', 'price: ','month: ', 'day: ', 'year: '], "Please insert data: ")
+    record.insert(ID_INDEX, common.generate_random(table))
+    table.append(record)
+    data_manager.write_table_to_file('sales/sales.csv', table)
     return table
 
 
@@ -75,7 +123,13 @@ def remove(table, id_):
     """
 
     # your code
-
+    ID_LIST_INDEX = 0
+    line_counter = 0
+    for row in table:
+        if row[ID_LIST_INDEX] == id_[ID_LIST_INDEX]:
+            table.remove(row)
+        line_counter += 1
+    data_manager.write_table_to_file('sales/sales.csv', table)
     return table
 
 
