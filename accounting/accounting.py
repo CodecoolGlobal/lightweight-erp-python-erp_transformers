70396244928f8bece50146a7bb95da9d26ecd30a
Michał Szymański <szyman.inc@gmail.com>
2020-01-09 13:48:14 +0100

    Accounting added

--- accounting/accounting.py
+++ accounting/accounting.py
@@ -29,7 +29,50 @@ def start_module():
         None
     """
 
-    # you code
+
+    options = ['Show table',
+                'Add a new record',
+                'Remove record',
+                'Update single record',
+                'Which year has the highest profit?',
+                'What is the average (per item) profit in a given year?']
+    dict_menu = {'1': show_table_wrapper,
+                '2': add_wrapper,
+                '3': remove_wrapper,
+                '4': update_wrapper,
+                '5': which_year_max_wrapper,
+                '6': avg_amount_wrapper}
+    common.sub_menu(dict_menu, options, "Accounting menu")
+
+
+def show_table_wrapper():
+    table = data_manager.get_table_from_file('accounting/items.csv')
+    show_table(table)
+
+
+def add_wrapper():
+    table = data_manager.get_table_from_file('accounting/items.csv')
+    add(table)
+
+
+def remove_wrapper():
+    table = data_manager.get_table_from_file('accounting/items.csv')
+    remove(table, ui.get_inputs(['ID :'], 'Enter ID: '))
+
+
+def update_wrapper():
+    table = data_manager.get_table_from_file('accounting/items.csv')
+    update(table, ui.get_inputs(['ID :'], 'Enter ID: '))
+
+
+def which_year_max_wrapper():
+    table = data_manager.get_table_from_file('accounting/items.csv')
+    which_year_max(table)
+
+
+def avg_amount_wrapper():
+    table = data_manager.get_table_from_file('accounting/items.csv')
+    avg_amount(table, ui.get_inputs(['Year'], 'Enter a year: '))
 
 
 def show_table(table):
@@ -44,6 +87,8 @@ def show_table(table):
     """
 
     # your code
+    titles_list = ['ID', 'Month of the transaction', 'Day of the transaction', 'Year of the transaction', 'in = income, out = outflow', 'Amount of transaction in USD']
+    ui.print_table(table, titles_list)
 
 
 def add(table):
@@ -58,7 +103,11 @@ def add(table):
     """
 
     # your code
-
+    ID_INDEX = 0
+    record = ui.get_inputs(['Month of the transaction: ', 'Day of the transaction: ', 'Year of the transaction: ', 'in = income, out = outflow: ', 'Amount of transaction in USD: '], "Please insert data: ")
+    record.insert(ID_INDEX, common.generate_random(table))
+    table.append(record)
+    data_manager.write_table_to_file('accounting/items.csv', table)
     return table
 
 
@@ -75,7 +124,11 @@ def remove(table, id_):
     """
 
     # your code
-
+    ID_LIST_INDEX = 0
+    for row in table:
+        if row[ID_LIST_INDEX] == id_[ID_LIST_INDEX]:
+            table.remove(row)
+    data_manager.write_table_to_file('accounting/items.csv', table)
     return table
 
 
@@ -92,7 +145,15 @@ def update(table, id_):
     """
 
     # your code
-
+    ID_LIST_INDEX = 0
+    iterate = 0
+    for row in table:
+        if row[ID_LIST_INDEX] == id_[ID_LIST_INDEX]:
+            updated_record = ui.get_inputs(['Month of the transaction: ', 'Day of the transaction: ', 'Year of the transaction: ', 'in = income, out = outflow: ', 'Amount of transaction in USD: '], row)
+            table[iterate] = updated_record
+            data_manager.write_table_to_file('accounting/items.csv', table)
+            break
+        iterate += 1
     return table
 
 
